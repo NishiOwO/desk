bbb534bb08fb7ac7d50f9a55bde984f67bedc8de
NishiOwO <nishi@nishi.boats>
2025-07-15 12:40:13 +0900

    adding example

---
+++ example.c
@@ -0,0 +1,11 @@
+#include <Desk/Desk.h>
+#include <Desk/Window.h>
+
+int main(int argc, char** argv){
+  DeskWidget w;
+  DeskInit();
+  w = DeskCreateWidget(DeskWindowWidgetClass, 1,
+    DeskKeyTitle, "window"
+  );
+  DeskMainLoop(w);
+}
